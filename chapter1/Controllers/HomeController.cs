cc9c132d1cdaebbc38a122f9db76deb45840ee28
Zahid <zahidnl@outlook.com>
2020-10-28 23:45:48 +0100

    Chapter2 done

---
+++ chapter1/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace chapter1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = System.DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView", viewModel);
+        }
+
+    }
+}
