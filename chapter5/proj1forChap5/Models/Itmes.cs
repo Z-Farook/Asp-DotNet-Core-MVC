ff74ee5369dc1e5155a9ee541f4e465da54683ee
Zahid <zahidnl@outlook.com>
2020-12-25 17:03:47 +0100

    Pattern Matching

--- chapter5/proj1forChap5/Models/Itmes.cs
+++ chapter5/proj1forChap5/Models/Itmes.cs
@@ -2,6 +2,27 @@ namespace proj1forChap5.Models
 {
     public class Item
     {
+        #region //Pattern Matching in switch Statements
+        public void MyMethod(string parameter)
+        {
+
+            object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 };
+            decimal total = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                switch (data[i])
+                {
+                    case /*Type check*/ decimal /*A local Variable */ decimalValue:
+                        total += decimalValue;
+                        break;
+                    case /*Type check*/ int /*A local Variable */ intValue /*condition key word:*/ when intValue > 50:
+                        total += intValue;
+                        break;
+                }
+            }
+        }
+        #endregion
+
         public string Name { get; set; }
         public decimal? Price { get; set; }
 
