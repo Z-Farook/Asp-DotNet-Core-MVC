8658953ce9a36487cf099b86d4186da8a6a804b2
Zahid <zahidnl@outlook.com>
2020-12-31 13:39:59 +0100

    Asynchronous Enumerable

--- chapter5/proj1forChap5/Controllers/AsyncAndAwaitController.cs
+++ chapter5/proj1forChap5/Controllers/AsyncAndAwaitController.cs
@@ -9,10 +9,35 @@ namespace proj1forChap5.Controllers
     //visit at: http://localhost:5000/AsyncAndAwait/index
     public class AsyncAndAwaitController : Controller
     {
+#if false
         public async Task<string> Index()
         {
             long? length = await MyAsyncMethods.GetPageLength();
             return $"Length: {length}";
         }
+#endif
+
+#if false
+        //Using the first method which waits for the whole completion oF HTTP request 
+         public async Task<List<string>> Index()
+        {
+            List<string> output = new List<string>();
+            foreach (long? len in await MyAsyncMethods.GetPageLengths(output, "apress.com", "microsoft.com", "amazon.com"))
+            {
+                output.Add($"Page length: { len}");
+            }
+            return output;
+        }
+#endif
+        public async Task<List<string>> Index()
+        {
+            List<string> output = new List<string>();
+            await foreach (long? len in MyAsyncMethods.GetPageLengths(output, "apress.com", "microsoft.com", "amazon.com"))
+            {
+                output.Add($"Page length: { len}");
+            }
+            return output;
+        }
+
     }
 }
--- chapter5/proj1forChap5/Models/MyAsyncMethods.cs
+++ chapter5/proj1forChap5/Models/MyAsyncMethods.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 namespace proj1forChap5.Models
@@ -22,5 +23,39 @@ namespace proj1forChap5.Models
             var httpMessage = await client.GetAsync("http://apress.com");
             return httpMessage.Content.Headers.ContentLength;
         }
+#if false
+        //Using an Asynchronous Enumerable 
+        //this method does not pass the result to the caller until all HTTP are done
+        public static async Task<IEnumerable<long?>> GetPageLengths(List<string> output, params string[] urls)
+        {
+            List<long?> results = new List<long?>();
+            HttpClient client = new HttpClient();
+            foreach (string url in urls)
+            {
+                output.Add($"Started request for {url}");
+                var httpMessage = await client.GetAsync($"http://{url}");
+                results.Add(httpMessage.Content.Headers.ContentLength);
+                output.Add($"Completed request for {url}");
+            }
+            return results;
+        }
+#endif
+        //Using an Asynchronous Enumerable 
+        // this method solve the problem above (the wating)
+        public static async IAsyncEnumerable<long?> GetPageLengths(List<string> output, params string[] urls)
+        {
+            List<long?> results = new List<long?>();
+            HttpClient client = new HttpClient();
+            foreach (string url in urls)
+            {
+                output.Add($"Started request for {url}");
+                var httpMessage = await client.GetAsync($"http://{url}");
+                results.Add(httpMessage.Content.Headers.ContentLength);
+                output.Add($"Completed request for {url}");
+                yield return httpMessage.Content.Headers.ContentLength;
+            }
+
+        }
+
     }
 }
