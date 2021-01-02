f399feb8adc24159da2421926ec5ef84a39034d2
Zahid <zahidnl@outlook.com>
2021-01-02 21:47:05 +0100

    Application Services and the Request Pipeline

--- chapter7/proj1forchap7/Startup.cs
+++ chapter7/proj1forchap7/Startup.cs
@@ -16,24 +16,34 @@ namespace proj1forchap7
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+#if false
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+#endif
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                /* To register the MVC Framework as a source of endpoints
+                calls the MapDefaultControllerRoute method */
+                endpoints.MapDefaultControllerRoute();
+
+                // endpoints.MapGet("/", async context =>
+                // {
+                //     await context.Response.WriteAsync("Hello World!");
+                // });
             });
         }
     }
