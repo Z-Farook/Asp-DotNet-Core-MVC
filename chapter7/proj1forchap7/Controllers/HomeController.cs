190fe87b12476ae16aa0988a40dcc533b9272821
Zahid <zahidnl@outlook.com>
2021-01-03 15:00:12 +0100

    Pagination

--- chapter7/proj1forchap7/Controllers/HomeController.cs
+++ chapter7/proj1forchap7/Controllers/HomeController.cs
@@ -1,14 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using proj1forchap7.Models;
+using System.Linq;
 namespace proj1forchap7.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
-        public IActionResult Index() => View(repository.Products);
+        // public IActionResult Index() => View(repository.Products);
+
+        // adding Pagination
+        public ViewResult Index(int productPage = 1)
+         => View(repository.Products
+         .OrderBy(p => p.ProductID)
+         .Skip((productPage - 1) * PageSize)
+         .Take(PageSize));
     }
 }
