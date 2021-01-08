f88f7d9ef86285df098e2edeb6ea39658f0a658c
Zahid <zahidnl@outlook.com>
2021-01-08 11:51:51 +0100

    Products Category test

--- chapter8/proj1forchap7_Test/HomeControllerTests.cs
+++ chapter8/proj1forchap7_Test/HomeControllerTests.cs
@@ -88,7 +88,7 @@ namespace proj1forchap7_Test
 
             // Act
             ProductsListViewModel result =
-                controller.ProductsWithPagination(2).ViewData.Model as ProductsListViewModel;
+                controller.ProductsWithPagination(null, 2).ViewData.Model as ProductsListViewModel;
 
             // Assert
             PagingInfo pageInfo = result.PagingInfo;
@@ -96,6 +96,31 @@ namespace proj1forchap7_Test
             Assert.Equal(3, pageInfo.ItemsPerPage);
             Assert.Equal(5, pageInfo.TotalItems);
             Assert.Equal(2, pageInfo.TotalPages);
+        }
+        [Fact]
+        public void Can_Filter_Products()
+        {
+            //Given
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(p => p.Products).Returns((new Product[]{
+                new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+                new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+                new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+                new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+                new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+            }).AsQueryable<Product>());
+
+            HomeController controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+
+            //When
+            Product[] result = (controller.ProductsWithPagination("Cat2", 1).ViewData.Model as ProductsListViewModel).Products.ToArray();
+
+            //Then
+            Assert.Equal(2, result.Length);
+            Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+
         }
 
     }
