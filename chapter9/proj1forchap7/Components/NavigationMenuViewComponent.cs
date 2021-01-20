4b86f7f1a7c070593e4dde57b5f073dcecc2ca71
Zahid <zahidnl@outlook.com>
2021-01-20 13:22:08 +0100

    added some commetns

--- chapter9/proj1forchap7/Components/NavigationMenuViewComponent.cs
+++ chapter9/proj1forchap7/Components/NavigationMenuViewComponent.cs
@@ -10,6 +10,11 @@ namespace proj1forchap7.Components
         {
             repository = repo;
         }
+        /// <summary>
+        /// The view component’s Invoke method is called when the component is used in a Razor view, and the result of the Invoke
+        /// method is inserted into the HTML sent to the browser
+        /// </summary>
+        /// <returns></returns>
         public IViewComponentResult Invoke()
         {
             /* dynamically assigned a SelectedCategory property to the ViewBag object and set its value to
