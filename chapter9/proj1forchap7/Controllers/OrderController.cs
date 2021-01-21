7282676f1dca44a13c5ce0e6b2257febc8408a30
Zahid <zahidnl@outlook.com>
2021-01-21 15:19:57 +0100

    Order's : interface, repo, DPI and controller

--- chapter9/proj1forchap7/Controllers/OrderController.cs
+++ chapter9/proj1forchap7/Controllers/OrderController.cs
@@ -1,12 +1,41 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using proj1forchap7.Models;
 using myModels = proj1forchap7.Models;
 namespace proj1forchap7.c
 {
     public class OrderController : Controller
     {
-        public ViewResult Checkout()
+        private myModels.IOrderRepository _repository;
+        private myModels.Cart _cart;
+
+        public OrderController(IOrderRepository repository, Cart cart)
         {
-            return View(new myModels.Order());
+            _repository = repository;
+            _cart = cart;
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (_cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("orderError", "Sorry your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = _cart.Lines.ToArray();
+
+                _repository.SaveOrder(order);
+
+                _cart.Clear();
+
+                return RedirectToPage("/Complated", new { orderId = order.OrderID });
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- chapter9/proj1forchap7/Startup.cs
+++ chapter9/proj1forchap7/Startup.cs
@@ -40,6 +40,7 @@ namespace proj1forchap7
             /* it is to create a service for the IStoreRepository
             interface that uses EFStoreRepository as the implementation class. */
             services.AddScoped<IStoreRepository, EFStoreRepository>();
+            services.AddScoped<IOrderRepository, EFOrderRepository>();
             services.AddRazorPages();
             services.AddDistributedMemoryCache();
 
