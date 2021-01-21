924fcd311c0ac7177bb31dd4aa83e9315d17667c
Zahid <zahidnl@outlook.com>
2021-01-21 17:46:59 +0100

    Get the order form and validate it upon submitting

--- chapter9/proj1forchap7/Controllers/OrderController.cs
+++ chapter9/proj1forchap7/Controllers/OrderController.cs
@@ -14,13 +14,15 @@ namespace proj1forchap7.Controllers
             _repository = repository;
             _cart = cart;
         }
+        // this used  for the HTTP get to get the Form
+        public ViewResult Checkout() => View(new Order());
 
         [HttpPost]
         public IActionResult Checkout(Order order)
         {
             if (_cart.Lines.Count() == 0)
             {
-                ModelState.AddModelError("orderError", "Sorry your cart is empty!");
+                ModelState.AddModelError("", "Sorry your cart is empty!");
             }
             if (ModelState.IsValid)
             {
