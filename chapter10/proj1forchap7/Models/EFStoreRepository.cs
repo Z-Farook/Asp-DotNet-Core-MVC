8e937c6da27d0a2a71b9d4ee37d67e571e77f43b
Zahid <zahidnl@outlook.com>
2021-02-04 10:45:34 +0100

    Crud methods for prodcut and the prodcut field validations

--- chapter10/proj1forchap7/Models/EFStoreRepository.cs
+++ chapter10/proj1forchap7/Models/EFStoreRepository.cs
@@ -11,5 +11,23 @@ namespace proj1forchap7.Models
         /* implimenting the interface here!!!
         * and the context.Products is POINTING to the StoreDbContext.Products*/
         public IQueryable<Product> Products => context.Products;
+
+        public void CreateProduct(Product p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(Product p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+
+        }
+
+        public void SaveProduct(Product p)
+        {
+            context.SaveChanges();
+        }
     }
 }
--- chapter10/proj1forchap7/Models/IStoreRepository.cs
+++ chapter10/proj1forchap7/Models/IStoreRepository.cs
@@ -4,5 +4,8 @@ namespace proj1forchap7.Models
     public interface IStoreRepository
     {
         IQueryable<Product> Products { get; }
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
     }
 }
